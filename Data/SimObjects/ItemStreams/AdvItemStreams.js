4099421b3af6a898c81cc4c6fa197035d9d936b2
Daimonfire1 <142779937+Daimonfire1@users.noreply.github.com>
2024-03-29 15:54:08 +0000

    timefortesties

--- Data/SimObjects/ItemStreams/AdvItemStreams.js
+++ Data/SimObjects/ItemStreams/AdvItemStreams.js
@@ -6,15 +6,33 @@ export class IOStream extends Item
     constructor(ItemDef, Amount, CycleTime, Unit){
         super(ItemDef, Amount)
         this.Flow = Amount / (CycleTime * TimeUnit[Unit])
-        
-        this.CycleTime = CycleTime
     }
 
-    static getItemFlow(Unit){
-        return this.Flow * TimeUnit[Unit]
+    static TimePerUnit(CycleTime, Unit){
+        return CycleTime * TimeUnit[Unit]
     }
 
-    static getMoneyFlow(Unit){
-        return this.getItemFlow(Unit) * this.Value()
+    static UpdateFlow(Amount, CycleTime, Unit){
+        this.Flow = Amount * this.TimePerUnit(CycleTime, Unit)
+    }
+
+    getItemFlow(){
+        return this.Flow
+    }
+    getItemFlow(CycleTime, Unit){
+        return this.Flow * TimePerUnit(CycleTime, Unit)
+    }
+
+    getMoneyFlow(){
+        return this.getItemFlow() * this.Value
+    }
+    getMoneyFlow(CycleTime, Unit){
+        return this.getItemFlow(CycleTime, Unit) * this.Value
+    }
+
+    updateInternals(ItemDef, Amount, CycleTime, Unit){
+        super.setnewitem(ItemDef)
+        super.setnewamount(Amount)
+        this.UpdateFlow(Amount, CycleTime, Unit)
     }
 }
