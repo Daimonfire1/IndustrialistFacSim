ffe5606067fa2ec77271b27583f2dde1ddf2c210
Daimonfire1 <142779937+Daimonfire1@users.noreply.github.com>
2024-03-29 15:24:30 +0100

    Aaaaab

--- Data/SimObjects/ItemStreams/AdvItemStreams.js
+++ Data/SimObjects/ItemStreams/AdvItemStreams.js
@@ -1,14 +1,18 @@
 import { ItemStream } from "./ItemStream";
 import { TimeUnit } from "../Values/Modifiers";
 
-export class InputStream extends ItemStream{
+export class IOStream extends ItemStream{
     constructor(ItemDef, Amount, CycleTime, Unit){
         this.Flow = Amount / (CycleTime * TimeUnit[Unit])
         super(ItemDef, this.Flow)
         this.CycleTime = CycleTime
     }
 
-    get Flow(Unit){
+    get ItemFlow(Unit){
         return this.Flow * TimeUnit[Unit]
+    }
+
+    get MoneyFlow(Unit){
+        return this.ItemFlow(Unit) * this.Value()
     }
 }
